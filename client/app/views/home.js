c5c2f77704c6c24be20af647c6ff7e7d0a88fce5
Thom Stilwill <thom.stilwill@gmail.com>
2017-12-16 19:57:40 -0500

    add links

--- client/app/views/home.js
+++ client/app/views/home.js
@@ -11,7 +11,6 @@ angular.module('ratel')
             init();
         }
 
-
         var clockDiv = $("#clock"),
             timeDiv = $("#time"),
             dayDiv = $("#day"),
@@ -21,60 +20,47 @@ angular.module('ratel')
             secondsDiv = $("#seconds"),
             tickDiv = $("#tick");
     
-        var bindEvents = function () {
+        function drawclock(){
+            var datetime = moment();
     
-            setInterval(function () {
-    
-                var datetime = moment();
-    
-                var time = datetime.format("h:mm a").toLowerCase();
-                timeDiv.text(time);
-    
-                var day = datetime.format("dddd");
-                dayDiv.text(day);
-    
-                var date = datetime.format("MMMM Do, YYYY");
-                dateDiv.text(date);
-    
-                var width = clockDiv.width();
-    
-    
-                var second = datetime.format("s");
-    
-                var secondWidth = Math.floor(width / 60);
-                var spacer = second * secondWidth;
-    
-                spacerDiv.width(spacer + "px");
-                tickDiv.width(secondWidth * 2);
-                tickDiv.height(secondWidth * 2);
-                secondsDiv.height(secondWidth);
-    
-            }, 500);
-    
-            $(window).on('resize', function () {
-                resize();
-            });
-    
-        };
-    
+            var time = datetime.format("h:mm a").toLowerCase();
+            timeDiv.text(time);
+
+            var day = datetime.format("dddd");
+            dayDiv.text(day);
+
+            var date = datetime.format("MMMM Do, YYYY");
+            dateDiv.text(date);
+
+            var width = clockDiv.width();
+            var second = datetime.format("s");
+
+            var secondWidth = Math.floor(width / 60);
+            var spacer = second * secondWidth;
+
+            spacerDiv.width(spacer + "px");
+            tickDiv.width(secondWidth * 2);
+            tickDiv.height(secondWidth * 2);
+            secondsDiv.height(secondWidth);
+        }
+
         function resize() {
             var options = { max: 200 };
-    
             timeDiv.quickfit(options);
             dayDiv.quickfit(options);
             dateDiv.quickfit(options);
         };
-    
+
+        function bindEvents() {
+            setInterval(drawclock, 500);
+            $(window).on('resize', resize);
+        };
     
         function init() {
-    
             bindEvents();
             setTimeout(function () {
                 resize();
             }, 500);
-
         };
-
-
     }
 })
