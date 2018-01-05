7c806f6d9640157d3066aa1b926e6ec9c2c69953
Thom Stilwill <thom.stilwill@gmail.com>
2018-01-05 11:42:48 -0500

    added clock

--- client/app/routes.js
+++ client/app/routes.js
@@ -1,57 +1,62 @@
 angular.module('ratel')
-.config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-   $urlRouterProvider.otherwise('/');
-   $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: true
-  });
-   
-   var states = [
-        {  name: 'home',
-            component: 'home',
-            resolve: {
-                links: function(LinkService){
-                    return LinkService.fetch();
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: true
+        });
+
+        var states = [{
+                name: 'home',
+                component: 'home',
+                resolve: {
+                    links: function (LinkService) {
+                        return LinkService.fetch();
+                    }
                 }
+            },
+            {
+                name: 'persons',
+                component: 'persons',
+                resolve: {
+                    model: function (PersonService) {
+                        return PersonService.fetch();
+                    }
+                }
+            },
+            {
+                name: 'about',
+                component: 'about'
+            },
+            {
+                name: 'clock',
+                component: 'clock'
             }
-        },
-        {  name: 'persons',
-           component: 'persons',
-           resolve: {
-               model: function(PersonService){
-                   return PersonService.fetch();
-               }
-           }
-       },
-       {  name: 'about',
-       component: 'about'
-   }
-    ]
+        ]
 
-    states.forEach(function(state){
-        $stateProvider.state(state);
-    })
+        states.forEach(function (state) {
+            $stateProvider.state(state);
+        })
 
-}])
-.run([ function() {
-    
-  }])
-.run(['$state', '$trace', '$transitions', function($state,$trace,$transitions) {
-    //$trace.enable('TRANSITION');
+    }])
+    .run([function () {
 
-    $state.defaultErrorHandler(function() {
-        console.log('Default error handler fired!');
-    });
+    }])
+    .run(['$state', '$trace', '$transitions', function ($state, $trace, $transitions) {
+        //$trace.enable('TRANSITION');
 
-    $transitions.onStart({}, function(transition) {
-        //console.log('Starting transition!');
-        //transition.promise.finally(console.log('Transition promise finally fired!'));
-    });
-    $transitions.onError({}, function(transition) {
-        console.log('Transition erred!');
-    });
+        $state.defaultErrorHandler(function () {
+            console.log('Default error handler fired!');
+        });
 
-    $state.go('home')
-}])
+        $transitions.onStart({}, function (transition) {
+            //console.log('Starting transition!');
+            //transition.promise.finally(console.log('Transition promise finally fired!'));
+        });
+        $transitions.onError({}, function (transition) {
+            console.log('Transition erred!');
+        });
 
+        $state.go('home')
+    }])
--- dist/web/app/app.js
+++ dist/web/app/app.js
@@ -8,63 +8,67 @@ angular.module('ratel', ['ui.router'])
 }]);
 
 angular.module('ratel')
-.config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-   $urlRouterProvider.otherwise('/');
-   $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: true
-  });
-   
-   var states = [
-        {  name: 'home',
-            component: 'home',
-            resolve: {
-                links: function(LinkService){
-                    return LinkService.fetch();
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: true
+        });
+
+        var states = [{
+                name: 'home',
+                component: 'home',
+                resolve: {
+                    links: function (LinkService) {
+                        return LinkService.fetch();
+                    }
                 }
+            },
+            {
+                name: 'persons',
+                component: 'persons',
+                resolve: {
+                    model: function (PersonService) {
+                        return PersonService.fetch();
+                    }
+                }
+            },
+            {
+                name: 'about',
+                component: 'about'
+            },
+            {
+                name: 'clock',
+                component: 'clock'
             }
-        },
-        {  name: 'persons',
-           component: 'persons',
-           resolve: {
-               model: function(PersonService){
-                   return PersonService.fetch();
-               }
-           }
-       },
-       {  name: 'about',
-       component: 'about'
-   }
-    ]
+        ]
 
-    states.forEach(function(state){
-        $stateProvider.state(state);
-    })
+        states.forEach(function (state) {
+            $stateProvider.state(state);
+        })
 
-}])
-.run([ function() {
-    
-  }])
-.run(['$state', '$trace', '$transitions', function($state,$trace,$transitions) {
-    //$trace.enable('TRANSITION');
+    }])
+    .run([function () {
 
-    $state.defaultErrorHandler(function() {
-        console.log('Default error handler fired!');
-    });
+    }])
+    .run(['$state', '$trace', '$transitions', function ($state, $trace, $transitions) {
+        //$trace.enable('TRANSITION');
 
-    $transitions.onStart({}, function(transition) {
-        //console.log('Starting transition!');
-        //transition.promise.finally(console.log('Transition promise finally fired!'));
-    });
-    $transitions.onError({}, function(transition) {
-        console.log('Transition erred!');
-    });
+        $state.defaultErrorHandler(function () {
+            console.log('Default error handler fired!');
+        });
 
-    $state.go('home')
-}])
+        $transitions.onStart({}, function (transition) {
+            //console.log('Starting transition!');
+            //transition.promise.finally(console.log('Transition promise finally fired!'));
+        });
+        $transitions.onError({}, function (transition) {
+            console.log('Transition erred!');
+        });
 
-
+        $state.go('home')
+    }])
 angular.module('ratel')
 .service('LinkService', ['$q','$http','$log', function($q,$http,$log){
 
@@ -151,6 +155,102 @@ angular.module('ratel')
     controller: function($scope){
         const ctrl = this;
         
+    }
+})
+angular.module('ratel')
+.component('clock',{
+    templateUrl: 'app/views/clock.html',
+    controller: function($scope){
+        const ctrl = this;
+
+        var currentLang;
+
+        function formatArray(array) {
+            return array.join('<br/>');
+        }
+    
+        function calendarHtml() {
+            var arr = [];
+            arr.push(moment().subtract('days', 10).calendar());
+            arr.push(moment().subtract('days', 6).calendar());
+            arr.push(moment().subtract('days', 3).calendar());
+            arr.push(moment().subtract('days', 1).calendar());
+            arr.push(moment().calendar());
+            arr.push(moment().add('days', 1).calendar());
+            arr.push(moment().add('days', 3).calendar());
+            arr.push(moment().add('days', 10).calendar());
+            return formatArray(arr);
+        }
+    
+        function formatHtml() {
+            var arr = [];
+            arr.push(moment().format('MMMM Do YYYY, h:mm:ss a'));
+            arr.push(moment().format('dddd'));
+            arr.push(moment().format("MMM Do YY"));
+            arr.push(moment().format('YYYY [escaped] YYYY'));
+            arr.push(moment().format());
+            return formatArray(arr);
+        }
+    
+        function fromHtml() {
+            var arr = [];
+            arr.push(moment("20111031", "YYYYMMDD").fromNow());
+            arr.push(moment("20120620", "YYYYMMDD").fromNow());
+            arr.push(moment().startOf('day').fromNow());
+            arr.push(moment().endOf('day').fromNow());
+            arr.push(moment().startOf('hour').fromNow());
+            return formatArray(arr);
+        }
+    
+        function langHtml() {
+            var arr = [];
+            arr.push(moment().format('L'));
+            arr.push(moment().format('l'));
+            arr.push(moment().format('LL'));
+            arr.push(moment().format('ll'));
+            arr.push(moment().format('LLL'));
+            arr.push(moment().format('lll'));
+            arr.push(moment().format('LLLL'));
+            arr.push(moment().format('llll'));
+            return formatArray(arr);
+        }
+    
+        function update() {
+            moment.lang(currentLang);
+    
+            $('#js-format').html(formatHtml());
+            $('#js-from-now').html(fromHtml());
+            $('#js-calendar').html(calendarHtml());
+            $('#js-lang').html(langHtml());
+    
+            var now = moment(),
+                second = now.seconds() * 6,
+                minute = now.minutes() * 6 + second / 60,
+                hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+    
+    
+            $('#hour').css("transform", "rotate(" + hour + "deg)");
+            $('#minute').css("transform", "rotate(" + minute + "deg)");
+            $('#second').css("transform", "rotate(" + second + "deg)");
+        }
+    
+        function timedUpdate() {
+            update();
+            setTimeout(timedUpdate, 1000);
+        }
+    
+        timedUpdate();
+    
+        var init = function () {
+            var dom = $(this);
+            currentLang = dom.data('lang');
+            $('[data-lang]').removeClass('active');
+            dom.addClass('active');
+            update();
+        };
+        
+
+        init()
     }
 })
 angular.module('ratel')
