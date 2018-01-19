deafad79534a294a300df2c5d59cfbec336e4f8b
Thom Stilwill <thom.stilwill@gmail.com>
2018-01-18 22:18:32 -0500

    login form

--- client/app/views/login.js
+++ client/app/views/login.js
@@ -3,5 +3,13 @@ angular.module('ratel')
     templateUrl: 'app/views/login.html',
     controller: function ($scope) {
       const ctrl = this
+
+      ctrl.submit = function (form) {
+        if (form.$invalid) {
+          console.log('invalid: ', form.$error)
+          return
+        }
+        console.log(form)
+      }
     }
   })
